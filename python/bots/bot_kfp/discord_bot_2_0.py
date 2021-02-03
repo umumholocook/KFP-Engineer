1171817f97bfda17d61be2cceacaa4b8925afc43
umumholocook <umumhololive@gmail.com>
2021-02-02 21:43:09 -0800

    remove token

--- python/bots/bot_kfp/discord_bot_2_0.py
+++ python/bots/bot_kfp/discord_bot_2_0.py
@@ -4,7 +4,7 @@ from discord.ext.commands import errors as disErros
 import asyncio, os
 import sys, traceback, json
 
-TOKEN = 'ODA2MzgxODE4ODQwMzUwNzMw.YBonlw.1Razl5F24ZFZ7xDyApiJtTLnxxw'
+TOKEN = ''
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix = '!',intents = intents)
