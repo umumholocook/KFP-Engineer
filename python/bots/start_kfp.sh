f65129d59efa42cd72d00818bdf8fc3484426d01
umumholocook <umumhololive@gmail.com>
2021-02-05 19:25:10 -0800

    update the run script

--- python/bots/start_kfp.sh
+++ python/bots/start_kfp.sh
@@ -1,2 +1,11 @@
-
+#!/bin/bash -e
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # check if python 3 exist
+    if [[ "$(python3 -V)" =~ "Python 3" ]]; then
+        echo "Running KFP bot on OSX with python 3"
+        python3 discord_bot_2_0.py
+        return
+    fi
+fi
+# else, run the default python
 python discord_bot_2_0.py
