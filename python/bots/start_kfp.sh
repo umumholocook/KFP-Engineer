d6f449767169c845795c186e56cf84392b2f805a
umumholocook <umumhololive@gmail.com>
2021-03-13 22:42:31 -0800

    fix start script

--- python/bots/start_kfp.sh
+++ python/bots/start_kfp.sh
@@ -3,9 +3,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # check if python 3 exist
     if [[ "$(python3 -V)" =~ "Python 3" ]]; then
         echo "Running KFP bot on OSX with python 3"
+        pkill -9 Python
         python3 main.py
         exit 0
     fi
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    pkill -9 Python
+    python3 main.py
+    exit 0
 fi
 pkill -9 Python
 # else, run the default python
