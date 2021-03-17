9cefb57653fe9e45548be0e7740d74b6d69e4e8f
umumholocook <umumhololive@gmail.com>
2021-03-16 21:04:52 -0700

    更新版本號

--- python/bots/main.py
+++ python/bots/main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from common.KFP_DB import KfpDb
 from common.ChannelUtil import ChannelUtil
 
-VERSION = "0.3.9"
+VERSION = "0.5.0"
 TOKEN=os.environ['KFP_TOKEN']
 intents = discord.Intents.default()
 intents.members = True
