2529aae3e447730d270cad4114a998c0cc890f9f
umumholocook <umumhololive@gmail.com>
2021-04-26 02:33:11 -0700

    disable hack

--- python/bots/main.py
+++ python/bots/main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from common.KFP_DB import KfpDb
 from common.ChannelUtil import ChannelUtil
 
-VERSION = "0.6.10"
+VERSION = "0.6.11"
 TOKEN=os.environ['KFP_TOKEN']
 intents = discord.Intents.default()
 intents.members = True
