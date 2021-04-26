0081b7b0fbb8f5cf2f9a35f89a74cd04cf4bbe43
umumholocook <umumhololive@gmail.com>
2021-04-26 00:53:20 -0700

    Update matcher to better handle role names

--- python/bots/data/DefaultRoleData.py
+++ python/bots/data/DefaultRoleData.py
@@ -5,41 +5,41 @@ KFP_DEFAULT = [
         "category": 1,
         "color":16777215,
         "level": 0,
-        "matcher": "雞蛋"
+        "matcher": "🥚雞蛋"
     },
     {
         "name":"🐤小雞🐤",
         "category": 1,
         "color":16753920,
         "level": 20,
-        "matcher": "小雞"
+        "matcher": "🐤小雞"
     },
     {
         "name":"🐓成雞🐓",
         "category": 1,
         "color":16758016,
         "level": 40,
-        "matcher": "成雞"
+        "matcher": "🐓成雞"
     },
     {
         "name":"🔥火雞🔥",
         "category": 1,
         "color":16762112,
         "level": 60,
-        "matcher": "火雞"
+        "matcher": "🔥火雞"
     },
     {
         "name":"🍗炸雞🍗",
         "category": 1,
         "color":16766208,
         "level": 80,
-        "matcher": "炸雞"
+        "matcher": "🍗炸雞"
     },
     {
         "name":"🔶👑不死鳥👑🔶",
         "category": 1,
         "color":16774400,
         "level": 100,
-        "matcher": "不死鳥"
+        "matcher": "🔶👑不死鳥"
     }
 ]
