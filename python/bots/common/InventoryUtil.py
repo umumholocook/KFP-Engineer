ffce248a6bfba2390fd879c32ade744a9de2dba3
Yue Lin <56903959+YueYueWoof@users.noreply.github.com>
2021-07-25 03:38:26 +0800

    expand shop abilities

--- python/bots/common/InventoryUtil.py
+++ python/bots/common/InventoryUtil.py
@@ -1,59 +1,102 @@
 from operator import le
 from common.models.InventoryRecord import InventoryRecord, ShopItem
 from common.models.InventoryRecord import Item
+from common.MemberUtil import MemberUtil
+
 
 class InventoryUtil():
 
     def addItemToShop(guild_id: int, item_name: str, amount: int = 1, hidden=False):
-        item = InventoryUtil.createItem(guild_id=guild_id, item_name=item_name)
-        ShopItem.create(
+        item: Item = InventoryUtil.searchItem(guild_id=guild_id, item_name=item_name)
+        shopItem: ShopItem = InventoryUtil.findShopItem(guild_id=guild_id, item_id=item)
+        if shopItem != -1:
+            shopItem.amount += amount
+            shopItem.save()
+        elif item != -1:
+            return ShopItem.create(
+                guild_id=guild_id,
+                item=item,
+                amount=amount,
+                hidden=hidden,
+            )
+        return -1
+
+    def searchItem(guild_id: int, item_name: str):
+        query = Item.select().where(
+            Item.guild_id == guild_id,
+            Item.name == item_name,
+        )
+        if query.exists():
+            item: Item = query.get()
+            return item
+        return -1
+
+    def createItem(guild_id: int, item_name: str, level_required: int = 10, price: int = 10, role_id: int = -1):
+        item = InventoryUtil.searchItem(guild_id=guild_id, item_name=item_name)
+        if item != -1:
+            return -1
+        return Item.create(
             guild_id=guild_id,
-            item=item,
-            amount=amount,
-            hidden=hidden,
+            name=item_name,
+            role_id=role_id,
+            token_required=price,
+            level_required=level_required,
         )
 
-    def createItem(guild_id: int, item_name: str, level_required: int = 10, price: int = 10, role_id: int = -1, hidden: bool = False):
-        return Item.create(
-            guild_id = guild_id,
-            name = item_name,
-            role_id = role_id,
-            token_required = price,
-            level_required = level_required,
-            hidden = hidden,
-        )
-    
+
     def createInventory(guild_id: int, user_id: int, item: Item, amount: int):
-        return InventoryRecord.create(
-            guild_id = guild_id,
+        return InventoryRecord.insert(
+            guild_id=guild_id,
             user_id=user_id,
             item=item,
             amount=amount
-        )
-
-    def addItemToUserInventory(guild_id: int, user_id: int, item_id: int, count: int):
-        itemRecord:InventoryRecord = InventoryUtil.findItemRecord(guild_id=guild_id, user_id=user_id, item_id=item_id)
-        if not itemRecord:    
-            itemRecord.amount += count
-            itemRecord.save()
-            return
-        itemRecord = InventoryRecord.insert(
-            guild_id = guild_id,
-            user_id = user_id,
-            item_id = item_id,
-            amount = count
         ).execute()
 
-    def getAvailableItems(guild_id: int):
-        result = []
-        query = Item.select().where(
-            Item.guild_id == guild_id,
-            Item.hidden == False)
+    def buyItem(guild_id: int, user_id: int, item_id: int, count: int):
+        shopItem = InventoryUtil.findShopItem(guild_id=guild_id, item_id=item_id)
+
+        # Cannot find products
+        if shopItem == -1:
+            return -1
+        member = MemberUtil.get_member(user_id)
+
+        # Token is less then price
+        if member.token < (shopItem.item.token_required * count):
+            return -2
+
+        # Transaction part
+        if shopItem.amount < count:
+            return -3
+        # shopItem is unlimited supply
+        elif shopItem.amount != -1:
+            shopItem.amount -= count
+        else:
+            pass
+        # Reduce member's token
+        spend = shopItem.item.token_required * count * -1
+        MemberUtil.add_token(member_id=user_id, amount=spend)
+        InventoryUtil.addItemToUserInventory(guild_id, user_id, item_id, count)
+        return 1
+
+    def findShopItem(guild_id: int, item_id: int):
+        query = ShopItem.select().where(
+            ShopItem.guild_id == guild_id,
+            ShopItem.item_id == item_id,
+        )
         if query.exists():
-            for record in query.iterator():
-                result.append(record)
-        return result
-    
+            shopItem: ShopItem = query.get()
+            return shopItem
+        return -1
+
+    def addItemToUserInventory(guild_id: int, user_id: int, item_id: int, count: int):
+        itemRecord: InventoryRecord = InventoryUtil.findItemRecord(guild_id=guild_id, user_id=user_id, item_id=item_id)
+        # if member does not have InventoryRecord
+        if itemRecord:
+            itemRecord.amount += count
+            itemRecord.save()
+        shopItem = InventoryUtil.findShopItem(guild_id=guild_id, item_id=item_id)
+        InventoryUtil.createInventory(guild_id=guild_id, user_id=user_id, item=shopItem.item, amount=count)
+
     def getAllItemsBelongToUser(guild_id: int, user_id: int):
         result = []
         query = InventoryRecord.select().where(
@@ -74,3 +117,24 @@ class InventoryUtil():
             itemRecord: InventoryRecord = query.get()
             return itemRecord
         return None
+
+    def ListAllItem(guild_id: int):
+        result = []
+        query = Item.select().where(
+            Item.guild_id == guild_id,
+        )
+        if query.exists():
+            for record in query.iterator():
+                result.append(record)
+        return result
+
+    def ShopMenu(guild_id: int):
+        result = []
+        query = ShopItem.select()
+        if query.exists():
+            for record in query.iterator():
+                result.append(record)
+        return result
+
+    def exchangeCoinToToken(guild_id: int,user_id: int):
+        member = MemberUtil.get_member(user_id)
