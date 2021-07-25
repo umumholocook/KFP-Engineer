1656f744262adbe4473911fbc81d50b2c4785c9e
UZO <UZO.859079@gmail.com>
2021-07-25 21:19:15 +0800

    Update KfpRPG.py

--- python/bots/common/models/KfpRPG.py
+++ python/bots/common/models/KfpRPG.py
@@ -1,5 +1,6 @@
 from common.models.BaseModel import BaseModel
 from common.models.Member import Member
+from common.models.InventoryRecord import Item
 from peewee import *
 
 class Character(BaseModel):
@@ -10,7 +11,13 @@ class Character(BaseModel):
     hp_max = IntegerField()
     mp_current = IntegerField()
     mp_max = IntegerField()
+    attack_basic = IntegerField()
+    # attack_bonus = ForeignKeyField()
+    defense_basic = IntegerField()
+    # defense_bonus = ForeignKeyField()
 
-class Gear(BaseModel):
+class GearParts(Item):
     pass
 
+class Gear(GearParts):
+    pass
