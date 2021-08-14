26b0036f6a3b00a8c19521b94d91e4e41cc85745
umum <umumhololive@gmail.com>
2021-08-14 13:59:52 -0700

    add remove feature

--- python/bots/cogs/Bank.py
+++ python/bots/cogs/Bank.py
@@ -48,6 +48,29 @@ class Bank(commands.Cog):
         message += "```"
 
         await ctx.send(message)
+    
+    @bank_group.command(name = 'remove')
+    async def bank_remove(self, ctx: commands.Context, coins: int, user: User):
+        if not ChannelUtil.hasChannel(ctx.guild.id, ctx.channel.id, Util.ChannelType.BANK):
+            return
+        if coins < 1:
+            await ctx.send("請不要來亂的好嗎?")
+            return
+        member = MemberUtil.get_or_add_member(self.bot.user.id)
+        nick = ctx.guild.get_member(user.id).nick
+        if member.coin < coins:
+            await ctx.send(f"'{nick}'擁有餘額: {member.coin}. 不足以扣除 {coins}")
+            return
+        MemberUtil.add_coin(self.bot.user.id, coins)
+        MemberUtil.add_coin(user.id, -1 * coins)
+        bank = MemberUtil.get_or_add_member(self.bot.user.id)
+        message = f"扣除'{nick}'的硬幣 '{coins}'並充公成功! 目前餘額為\n"
+        message += "```"
+        message += f"銀行: {bank.coin}\n"
+        message += f"{nick}: {member.coin}\n"
+        message += "```"
+
+        await ctx.send(message)
 
 def setup(client):
     client.add_cog(Bank(client))
