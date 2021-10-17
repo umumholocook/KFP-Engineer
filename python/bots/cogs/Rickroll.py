d3867be5efdab84dd36144bf6a85735fb4938c24
umumholocook <78272843+umumholocook@users.noreply.github.com>
2021-10-17 01:53:58 -0700

    add rickroll

---
+++ python/bots/cogs/Rickroll.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from discord import Embed, File, User
+from common.Util import Util
+from common.RickrollGenerator import RickrollGenerator
+
+class Rickroll(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group(name = 'rick', invoke_without_command=True)
+    @commands.cooldown(1, 30, type=commands.BucketType.user)
+    async def rick_group(self, ctx:commands.Context, user:User):
+        avatar = Util.downloadUserAvatar(user=user)
+        imagePath = RickrollGenerator.createGif(avatar)
+
+        embedMsg = Embed()
+        embedMsg.set_image(url='attachment://rickrolled.gif')
+
+        img = File(imagePath, filename="rickrolled.gif")
+        await ctx.send(file=img, embed=embedMsg)
+        
+
+    @rick_group.error
+    async def rick_error(self, ctx:commands.Context, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            msg = "請勿過於頻繁使用本指令"
+            await ctx.reply(msg)
+        else:
+            raise error
+
+def setup(client):
+    client.add_cog(Rickroll(client))
---
+++ python/bots/common/RickrollGenerator.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageSequence
+from resource.data.rick_roll import POSITION
+import os, io
+
+from common.Util import Util
+
+class RickrollGenerator():
+    _avatar_size = 94
+
+    def getRickrollPath():
+        return os.sep.join((os.getcwd(), "resource", "image", "rickroll", "rick.gif"))
+    
+    def _getMaskImagePath():
+        return os.sep.join((os.getcwd(), "resource", "image", "rickroll", "mask.png"))
+
+    def getRickrollOutPath():
+        # return os.sep.join((tempfile.gettempdir(), "rickrolled.gif"))
+        return "rickrolled.gif"
+
+    def createGif(img: Image):
+        avatar = img.resize((RickrollGenerator._avatar_size, RickrollGenerator._avatar_size))
+        avatar = Util.createCircle(avatar, RickrollGenerator._getMaskImagePath())
+        avatar = Util.enlargeImage(avatar)
+        rick = Image.open(RickrollGenerator.getRickrollPath())
+        frames = []
+        index = 0
+        for frame in ImageSequence.Iterator(rick):
+            x = POSITION[index]["x"]
+            y = POSITION[index]["y"]
+            d = POSITION[index]["degree"]
+            avatarR = Util.rotateImage(avatar, d)
+            frame = frame.convert("RGBA")
+            frame.paste(avatarR, (x - int(RickrollGenerator._avatar_size / 2 + 20), y - int(RickrollGenerator._avatar_size / 2 + 20)), avatarR)
+
+            b = io.BytesIO()
+            frame.save(b, format="GIF")
+            frame = Image.open(b)
+
+            frames.append(frame)
+            index += 1
+        frames[0].save(RickrollGenerator.getRickrollOutPath(), save_all=True, append_images=frames[1:])
+        return RickrollGenerator.getRickrollOutPath()
